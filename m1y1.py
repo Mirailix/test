55cb4fccbfe9a172d1c1831db90d0fac7ea997f2
Mirailix <leha.goremykin.03@mail.ru>
2026-07-23 11:52:01 +0300

    Добавлен игрок

---
+++ m1y1.py
@@ -0,0 +1,43 @@
+print("hello world")
+
+slovo="Привет"
+slovo2=" я Леша"
+itog=slovo*6
+print(itog)
+print(slovo2)
+
+print(slovo,slovo2," мне 23 года и я учитель EasyCode")
+
+a = 5
+b = 6
+
+print(a+b)
+print(a/b)
+print(a*b)
+print(a-b)
+
+c=a+b  #Переменная 
+
+a=1
+print(a/2) # Получается ДРОБНОЕ ЧИСЛО
+
+print("На"*3)
+
+a=int(input("Введите число "))
+
+print(11//3)
+
+# 10>5  True
+# 10<5 False
+
+dict={
+    "name": "Коля",
+    "grades": [5, 4, 5, 3],
+    "address": {"city": "СПб", "street": "Невский"}
+}
+
+a=dict.get("name")
+print(a)
+
+
+    
